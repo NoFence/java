d406258427390e785ea36bef227b4fbba8980640
JamesLee <fight2night@naver.com>
2015-05-18 09:17:45 +0900

    Update PseudoCalculator.java

--- PseudoCalculator.java
+++ PseudoCalculator.java
@@ -17,20 +17,20 @@ class PseudoCalculator{
         expression = inputExpression();
         
        
-        StringTokenizer tokenizedString = new StringTokenizer(expression,"+-*/",true);
-        int numberOfToken = tokenizedString.countTokens();
+        StringTokenizer tokenizedString = new StringTokenizer(expression,"+-*/",true); 
+        int numberOfToken = tokenizedString.countTokens();  // 토큰의 갯수를 구하다
         
-        String tokenArray[];
+        String tokenArray[]; // 토큰을 담을 배열을 생성하다
         tokenArray = new String[numberOfToken]; 
         
-        int operandArray[] = new int[numberOfToken];
+        int operandArray[] = new int[numberOfToken]; // 피연산자를 담을 배열을 생성하다
         
-        state = transformTokenToInteger(tokenArray,operandArray,tokenizedString,numberOfToken);
+        state = transformTokenToInteger(tokenArray,operandArray,tokenizedString,numberOfToken);   // 예외 사항에 대한 상태 정보를 담다
         if(!state)
-           continue;
+           continue;  // 예외 상태 발생 시 계속해서 수식을 입력 받다
         
-        result = getResult(operandArray,tokenArray,numberOfToken);
-        outputExpression(result);
+        result = getResult(operandArray,tokenArray,numberOfToken);  // 수식에 대한 처리 과정을 반환 값으로 넘겨 받다
+        outputExpression(result);  // 반환 된 값을 출력하다
         
         }
     
@@ -60,9 +60,24 @@ class PseudoCalculator{
         tokenArray[i] = tokenizedString.nextToken();
         
         try {
-                 operandArray[i] = Integer.parseInt(tokenArray[i]);
+                 operandArray[i] = Integer.parseInt(tokenArray[i]);  // 토큰 배열에 담겨 있는 값이 숫자가 아니라면 예외가 발생, 이에 대한 예외 처리를 하다
             } 
+        
+       
          catch (Exception e) {
+            
+                
+                // 연산자는 항상 홀수 열에 존재하여야 한다
+                if ( (i%2) == 0 ){
+                        if ("+".equals(tokenArray[i]) || "*".equals(tokenArray[i]) || "-".equals(tokenArray[i]) || "/".equals(tokenArray[i])){
+                                System.out.println("올바른 값을 입력해 주세욧!");
+                                System.out.println("연산자가 순서가 올바르지 않습니다.");
+                                System.out.println();
+                                state = false;
+                                break;
+                }
+                  
+                // 사칙 연산자가 아닌 모든 기호는 예외 처리의 대상이다
                 if ( !("*".equals(tokenArray[i]) || "/".equals(tokenArray[i]) || "-".equals(tokenArray[i]) || "+".equals(tokenArray[i])  )  ) {
                     System.out.println("올바른 값을 입력해 주세욧!");
                     System.out.println("연산자와 숫자를 제외한 문자는 입력 될 수 없습니다.");
@@ -71,39 +86,32 @@ class PseudoCalculator{
                     break;
                            
                 }
-                
-                if ( (i%2) == 0 ){
-                        if ("+".equals(tokenArray[i]) || "*".equals(tokenArray[i]) || "-".equals(tokenArray[i]) || "/".equals(tokenArray[i])){
-                                System.out.println("올바른 값을 입력해 주세욧!");
-                                System.out.println("연산자는 연속으로 두 개 이상 입력 될 수 없습니다.");
-                                System.out.println();
-                                state = false;
-                                break;
-                }
-                  
          }
             
         }
            
+         //홀수 열에 피연산자만 담을 수 있도록 하다 
          if ("+".equals(tokenArray[i]) || "*".equals(tokenArray[i]) || "-".equals(tokenArray[i]) || "/".equals(tokenArray[i])){
                 continue;
          }
            
         
           
-}
+    }
         
        
         return state;
-    }
+}
     
     public static int getResult(int operandArray[],String tokenArray[], int numerOfToken){
-        int result;
-        int j=1;
-        int i;
+        int result; // 연산을 통해 변경 된 값을 누적하는 변수
+        int j=1; // 홀수 열에 담긴 배열의 값 처리를 위한 반복 제어 변수
+        int i; // 짝수 열에 담긴 배열의 값 처리를 위한 반복 제어 변수
         
-        result = operandArray[0];
+        result = operandArray[0];   // 피연산자 배열에 담긴 첫번째 원소를 담다.
             
+            
+            // 짝수 열에 담긴 숫자를 홀수 열에 담긴 연산자를 통해 연산하여 변경 된 값을 누적하다
             for(i=2; i < numerOfToken; i+=2){
                 if ("+".equals(tokenArray[j])){
                     result += operandArray[i];
@@ -132,7 +140,8 @@ class PseudoCalculator{
                     result -= operandArray[i];
                 }
                     
-                j+=2;
+                
+                j+=2; 
             }
         return result;
     }
